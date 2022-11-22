5b39f52a07fd8ab202c208ce7eaf8d5dce03aa2b
unknown <tyaps002@gmail,com>
2022-11-22 23:39:29 +0100

    Create login and sign page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  resources :expenditures
-  resources :groups
-  resources :users
+  devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+  # resources :expenditures
+  # resources :groups
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "home#index"
+   root "splash#index"
+   resources :users, only: [:index]
+    resources :groups, only: [:index, :show, :edit, :update, :destroy]
+     resources :expenditures, only: [:index, :show, :edit, :update, :destroy]
 end
